02f73f270e993814410655edcdb6066b4a0b1a25
mammaJustKillAMan <nata.jalosinska@gmail.com>
2025-08-09 22:08:29 +0200

    comment added

--- Animals.py
+++ Animals.py
@@ -10,6 +10,7 @@ def load_animals(filename):
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
             for key in line:
+                #other than few values the rest is boolean
                 if key != 'animal_name' and key != 'fins' and key != 'legs' and key != 'class_type':
                     line[key] = str_to_bool(line[key])
             animals[line['animal_name']]=line
