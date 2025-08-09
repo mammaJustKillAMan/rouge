d9fba0ddd3d0838b2476bdb56d5fdc0f0b19a436
mammaJustKillAMan <nata.jalosinska@gmail.com>
2025-08-09 22:00:21 +0200

    correction

--- Animals.py
+++ Animals.py
@@ -9,7 +9,7 @@ def load_animals(filename):
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
             for key in line:
-                if key != 'animal_name':
+                if key != 'animal_name' and key != 'fins' and key != 'legs' and key != 'class_type':
                     line[key] = str_to_bool(line[key])
             animals[line['animal_name']]=line
     return animals
